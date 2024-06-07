9bf174320471e97375c37e53899dbe6893de232f
Tess <tess.mueske@gmail.com>
2024-06-07 17:41:34 +0200

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title = "And Then There Were None", page_count = 272):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        """The title property"""
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        """"page_count must be an integer"""
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        """Prints a message indicating the page is being turned"""
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = "Adidas", size = 9):
+        self.brand = brand
+        self.size = size
+        self._condition = "New"
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        """Repairs the shoe and sets the condition to 'New'."""
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    @property
+    def condition(self):
+        """The condition property"""
+        return self._condition
+
+    @condition.setter
+    def condition(self, condition):
+        self._condition = condition
